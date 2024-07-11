4d09fa78950f07db86fb4e0dcf3e1dd484686e36
cygan3107 <krzysztof.malon@onet.pl>
2024-07-11 11:47:55 +0200

    first

---
+++ src/main.js
@@ -0,0 +1,115 @@
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Opcjonalny import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Opisany w dokumentacji
+import iziToast from "izitoast";
+// Opcjonalny import stylów
+import "izitoast/dist/css/iziToast.min.css";
+import ApiService from './api-service';
+import scrollMonitor from 'scrollmonitor';
+
+
+iziToast.show({
+  position: 'right-bottom',
+});
+
+//iziToast.info({
+ // backgroundColor: 'rgba(0,0,0,0.3)',
+  //svgColor: 'rgb(60, 197, 218)',
+  //clickToClose: false,
+//});
+
+const formRef = document.getElementById('search-form');
+const imageContainerRef = document.querySelector('.gallery');
+
+const search = new ApiService();
+const lightBox = new SimpleLightbox('.gallery a');
+
+const scrollListener = scrollMonitor.create(imageContainerRef);
+scrollListener.partiallyExitViewport(loadMore);
+
+formRef.addEventListener('submit', submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  imageContainerRef.innerHTML = '';
+  //iziToast.progress();
+  search.searchQuery = formRef.elements.searchQuery.value;
+  search.resetPage();
+  addImageAndUpdateUI();
+}
+
+function loadMore() {
+  if (search.isMorePage()) {
+    //iziToast.progress();
+    addImageAndUpdateUI();
+    return;
+  }
+  iziToast.info({message: "We're sorry, but you've reached the end of search results."});
+}
+
+async function addImageAndUpdateUI() {
+  try {
+    const image = await search.fetchImage();
+    if (search.currentPage === 1 && search.totalHits !== 0) {
+      iziToast.success({message:`Hooray! We found ${search.totalHits} images.`});
+    }
+
+    renderImage(image.hits);
+  } catch {
+    iziToast.error({message: 'Oops! Something went wrong! Try to reload the page!'});
+  }
+}
+
+function renderImage(array) {
+  if (!array.length) {
+    iziToast.error(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    iziToast.hide();
+    return;
+  }
+  const markup = array
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+        <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+                <b>Likes</b>
+                ${likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b>
+                ${views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b>
+                ${comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads</b>
+                ${downloads}
+              </p>
+            </div></a>
+          </div>`;
+      }
+    )
+    .join('');
+  imageContainerRef.insertAdjacentHTML('beforeend', markup);
+  lightBox.refresh();
+
+  iziToast.hide();
+
+  search.addPage();
+}
